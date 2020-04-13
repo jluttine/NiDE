99a42d9bc914e84514dcd69a09289269f1905ac6
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2020-04-13 14:03:29 +0300

    Remove font configuration from core

--- src/core.nix
+++ src/core.nix
@@ -15,27 +15,11 @@ in {
 
   config = lib.mkIf cfg.enable {
 
-    # Add DejaVu font and same fonts as Plasma
-    fonts.fonts = with pkgs; [
-      dejavu_fonts
-      noto-fonts
-      hack-font
-    ];
-
-    # These are the same fonts as Plasma uses
-    fonts.fontconfig.defaultFonts = {
-      monospace = [ "Hack" "Noto Mono" ];
-      sansSerif = [ "Noto Sans" ];
-      serif = [ "Noto Serif" ];
-    };
-
     # There also is the (new) i3-dmenu-desktop which only displays applications
     # shipping a .desktop file. It is a wrapper around dmenu, so you need that
     # installed.
     services.xserver.desktopManager.nide.i3Config = ''
       set $mod Mod4
-
-      font pango:DejaVu Sans Mono 8
 
       bindsym $mod+space exec ${pkgs.rofi}/bin/rofi -show drun -modi drun#run -matching fuzzy -show-icons
 
