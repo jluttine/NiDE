864d3fa0716ad71154a1d832fe9f04205517b6c4
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2020-04-13 14:05:45 +0300

    Make monitor layout automation optional

--- src/monitor.nix
+++ src/monitor.nix
@@ -4,9 +4,18 @@ let
   cfg = config.services.xserver.desktopManager.nide;
 in {
 
-  options = {};
+  options = with lib; {
+    services.xserver.desktopManager.nide.monitorLayout = {
+      automation = {
+        enable = mkOption {
+          type = types.bool;
+          default = true;
+        };
+      };
+    };
+  };
 
-  config = lib.mkIf cfg.enable {
+  config = lib.mkIf (cfg.enable && cfg.monitorLayout.automation.enable) {
 
     services.autorandr.enable = true;
 
